300eb3c3035b8b5162ace9a5e6f18f89b97b0312
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-03-28 18:46:32 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+// Created by: Fetuha
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle() {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle) * 0.5
+
+  // output
+  document.getElementById("area").innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+}
